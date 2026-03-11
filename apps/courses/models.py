1efd4544e55d512b2ff5ba41312c549b6759b6f7
Mahedi Hasan Noyon <noyonhasan320@gmail.com>
2026-03-11 16:07:52 +0600

    Written order model and payment model

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -67,7 +67,7 @@ class Course(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return self.title
     
@@ -106,9 +106,7 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
     
-
-
-
+# Reviews model ------
 class Review(models.Model):
     course=models.ForeignKey(Course,on_delete=models.CASCADE, related_name="reviews")
     user=models.ForeignKey(User,on_delete=models.CASCADE , related_name="reviews")
@@ -120,7 +118,6 @@ class Review(models.Model):
     
     class Meta:
         unique_together = ["course", "user"]
-
 
 
 
