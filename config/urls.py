b8583a75a02c7abd6a3604c0e25eb2d542564216
Sadiqul  Islam <sadiqul@joinventureai.com>
2026-03-12 11:30:42 +0600

    configure apps with project

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,53 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions, settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="From-Cert API",
+        default_version='v1',
+        description="API documentation for From-Cert",
+        terms_of_service="https://www.from-cert.com/terms/",
+        contact=openapi.Contact(email="mdsadiqulislam446@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # API Endpoints
+    path('api/', include([
+        path('users/', include('apps.users.urls')),
+        path('courses/', include('apps.courses.urls')),
+        path('affiliates/', include('apps.affiliates.urls')),
+        path('analytics/', include('apps.analytics.urls')),
+        path('payments/', include('apps.payments.urls')),
+        path('notifications/', include('apps.notifications.urls')),
+        path('enrollments/', include('apps.enrollments.urls')),
+        path('orders/', include('apps.orders.urls')),
+        path('messaging/', include('apps.messaging.urls')),
+    ]))
 ]
+
+
+# Swagger and Redoc UI (only in DEBUG mode)
+if settings.DEBUG:
+    urlpatterns += [
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+        path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    ]
+    
+# Static and Media files (only in DEBUG mode)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
