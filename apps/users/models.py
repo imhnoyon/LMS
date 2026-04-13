2b4935724891206ceba2640d97121e8e741e5024
Mahedi Hasan Noyon <noyonhasan320@gmail.com>
2026-04-13 12:48:39 +0600

    Add phone and avatar fields to User model; create stripe webhook setup

--- apps/users/models.py
+++ apps/users/models.py
@@ -67,6 +67,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     phone = models.CharField(max_length=20, blank=True)
     avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    
 
     is_verified = models.BooleanField(default=False)
     accepted_terms = models.BooleanField(default=False)
